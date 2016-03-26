86e6378ab8b40b10faa861af1f02de85436678b3
Lili Serfaty <lili.serfaty@gmail.com>
2016-03-25 21:27:50 -0500

    Add dog model, route, controller, views

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,21 @@
+class DogsController < ApplicationController
+  
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    Dog.create(dog_params)
+    redirect_to dogs_path
+  end
+
+  private
+  
+  def dog_params
+    params.require(:dog).permit(:name, :description)
+  end
+end
